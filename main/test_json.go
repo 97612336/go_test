53808d9b8dfd1c6122d77c6f61176898579e6b0a
wangkun <wangkun@autoforce.net>
2018-09-26 16:04:48 +0800

    增加测试字符串的功能

--- main/test_json.go
+++ main/test_json.go
@@ -3,12 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 func main() {
-	one_str := "[\"1\",\"123\"]"
+	one_str := "['1','2','这是字符串']"
+
+	new_one_str := strings.Replace(one_str, "'", "\"", -1)
+
 	var one_list []string
-	err := json.Unmarshal([]byte(one_str), &one_list)
+	err := json.Unmarshal([]byte(new_one_str), &one_list)
 	if err != nil {
 		fmt.Println(err)
 	}
