a3812a1f781dc7201532ed9996d64c12b5502f1f
wangkun <wangkun@autoforce.net>
2018-10-16 11:32:47 +0800

    增加go语言下建立静态文件目录的功能

---
+++ handlers/company_pro/test_static.go
@@ -0,0 +1,7 @@
+package company_pro
+
+import "net/http"
+
+func Get_static_file(w http.ResponseWriter, r *http.Request) {
+
+}
--- handlers/urls.go
+++ handlers/urls.go
@@ -9,4 +9,6 @@ import (
 func MyUrls() {
 	http.HandleFunc("/v1/get_one_specs", company_pro.Get_one_specs)
 	http.HandleFunc("/v1/send", email.Send_one_email)
+	//go语言设置静态文件目录
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("/home/wangkun/static_dir"))))
 }
